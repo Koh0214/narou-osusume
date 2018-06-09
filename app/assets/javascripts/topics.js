290eed213fd5619ce05cc1d7802a927f13fff63f
Kohei0214 <kkishimotok@gmail.com>
2018-06-10 00:32:14 +0900

    add-comment-reply-button and modal

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -1,3 +1,16 @@
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+$(function(){
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+  $('#myReplyModal').on('shown.bs.modal', function () {
+    $('#myReplyInput').trigger('focus')
+  })
+
+  $('#comment-reply-button').click(function(){
+    console.log('11111')
+    alert("aaa")
+    $('.reply-modal-text').val('>>')
+  })
+
+}
