ca1916f57bef39fd934d739208efebdeb1867a7d
Kohei0214 <kkishimotok@gmail.com>
2018-06-10 01:55:26 +0900

    remove config.assets.initialize_on_precompile = false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.assets.initialize_on_precompile = false
 end
