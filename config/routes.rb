8e4a7e01ba51b874331ddc316bd3c6e49dd46bae
Kohei0214 <kkishimotok@gmail.com>
2018-06-11 23:42:33 +0900

    routes.rb | add like-create resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   resources :topics do
-    resources :comments
+    resources :comments do
+      resources :likes, only: [:create]
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'topics#index'
