96f325c3469ffd7abf72aa88d11f1c78e4983f0d
Kohei0214 <kkishimotok@gmail.com>
2018-06-11 16:55:44 +0900

    add id_number when reply, and focus(only pc)
    PCのみ、モーダルにフォーカスができた。
    スマホは制限がかかっていて出来ないらしいけど、やりたいなと思う。
    
    タップを偽装したりして作れるかな？？
    
    遅延実行でフォーカスできた。
    https://ziyudom.com/js-delay/

--- app/assets/javascripts/topics-comment.js
+++ app/assets/javascripts/topics-comment.js
@@ -1,5 +1,24 @@
 $(function(){
-  $("#reply-button").on("click", function(){
-    $(".reply-modal-text").val("aaaaa");
+  // TODO わかりやすい名前をつけて関数化したい
+  // TODO スマホ時にモーダル自動フォーカスを入れたい
+  // 返信ボタン押したところのID番号を返信フォームに打ち込む
+  $(".reply-button").on("click", function(){
+    var id_number = $(this).parent().find(".row").find(".comment-id-number").text();
+    var reply_number = ">> " + id_number + "\r\n";
+    $(".reply-modal-text").val(reply_number);
+
+    //一秒後に実行
+    setTimeout(function(){
+      $('.reply-modal-text').focus();
+    },500);
   })
+
+  // コメント自動フォーカス
+  $(".comment-button").on("click", function(){
+    //一秒後に実行
+    setTimeout(function(){
+      $('.modal-text').focus();
+    },500);
+  })
+
 })
