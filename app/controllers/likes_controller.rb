d9c457dc5e16bd8965fb0e98cc38c7c9c4cfb497
Kohei0214 <kkishimotok@gmail.com>
2018-06-12 13:49:11 +0900

    remove binding.pry

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,8 @@
 class LikesController < ApplicationController
   def create
-    # binding.pry
     @like = Like.create(topic_id: params[:topic_id], comment_id: params[:comment_id])
     @topic = Topic.where(:id => params[:topic_id]).first
     @comment = Comment.where(:id => params[:comment_id]).first
-    # binding.pry
     if @like.save
       flash[:notice] = "Like was removed."
     else
