579081316facd5163a19173f539d5d98f11074d7
Kohei0214 <kkishimotok@gmail.com>
2018-06-14 11:04:12 +0900

    fix comment order

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,7 +12,7 @@ class TopicsController < ApplicationController
   # GET /topics/1.json
   def show
     @topic = Topic.where(:id => params[:id]).first
-    @comments = @topic.comments.all
+    @comments = @topic.comments.all.order("created_at")
     @comment = @topic.comments.new
   end
 
