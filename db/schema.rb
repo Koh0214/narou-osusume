a2eab50536e28aa4bd4aabbdf1e5cc09bd323e6c
Kohei0214 <kkishimotok@gmail.com>
2018-06-14 20:30:02 +0900

    install and migrate impressionist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_11_144845) do
+ActiveRecord::Schema.define(version: 2018_06_14_112926) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "topic_id", limit: 500
